5699b14c32b6b09b681513382610198d9c7ae569
Sydney Somerfield <sydneysomerfield@gmail.com>
2017-03-06 11:20:23 -0800

    3.6.17 Some stubbing

---
+++ connect-four.py
@@ -0,0 +1,35 @@
+"""Reads a move history file and 'plays' the moves in a graphic"""
+
+def read_file_to_list_of_moves(file_name):
+
+def create_game_board_graphic():
+
+def read_move_from_list(list_of_moves):
+    """Read move from list and determine token placement
+
+    :param list_of_moves:
+    :return: token placement
+    """
+def translate_to_game_board(token_placement):
+    """Place token on the game board
+
+    :param token_placement:
+    :return: graphic instruction
+    """
+# def check_horizontal_four(tokens_placement):
+#
+# def check_vertical_four(tokens_placement):
+#
+# def check_diagonal_four(tokens_placement):
+#
+# def determine_winner(horizontal_four, vertical_four, diagonal_four):
+#     """
+#
+#     :param horizontal_four:
+#     :param vertical_four:
+#     :param diagonal_four:
+#     :return:
+#     """
+
+
+
