fe5455dd89aaeec25cc58b857312469128838117
Sydney Somerfield <sydneysomerfield@gmail.com>
2017-03-08 22:20:42 -0800

    3.8.17 Added heaviest day of rain function

--- rain.py
+++ rain.py
@@ -22,6 +22,7 @@ def read_file_to_list(file_name):
                 date_string = convert_date_to_std_format(rain_info[0])
                 # add reformatted date back into the string
                 rain_info[0] = date_string
+                rain_info[1] = int(rain_info[1])
                 rainfall_dict.update({rain_info[0]:rain_info[1]})
     return rainfall_dict
 
@@ -38,13 +39,42 @@ def convert_date_to_std_format(date_string):
     std_date_string = year + '-' + month_num + '-' + day
     return std_date_string
 
-def day_of_year_with_most_avg_rainfall(rainfall_dict):
-    
+def day_with_most_rain(rainfall_dict):
+    rainfall_tenths = []
+    for tenths in rainfall_dict.values():
+        rainfall_tenths.append(tenths)
+    max_value = max(rainfall_tenths)
+
+    heaviest_rainfall_dates = []
+    for date, tenths in rainfall_dict.items():
+        if tenths == max_value:
+            heaviest_rainfall_dates.append(date)
+    max_value = [str(max_value)]
+    max_rain_with_dates = max_value + heaviest_rainfall_dates
+
+    return max_rain_with_dates
+
+def year_with_most_rain(rainfall_dict):
+
+
 
 def main():
 
     file_name = 'little_rain.txt'
-    print(read_file_to_list(file_name))
+    rainfall_dict = read_file_to_list(file_name)
+    print(rainfall_dict)
+
+    max_rain_with_dates = day_with_most_rain(rainfall_dict)
+    if len(max_rain_with_dates) == 2:
+        print('The day with the most rainfall on record is ' + max_rain_with_dates[1] +
+              ' with ' + max_rain_with_dates[0] + ' tenths of an inch of rain.')
+    else:
+        print('The days with the most rainfall on record are ', end='')
+        for index in max_rain_with_dates[1:]:
+            print(index + ', ', end='')
+        print(' with ' + max_rain_with_dates[0] + ' tenths of an inch of rain')
+
+
 
 if __name__ == '__main__':
     main()
