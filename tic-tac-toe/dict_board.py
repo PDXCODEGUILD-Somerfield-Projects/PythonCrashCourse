c826109558f799b41d0ae9363fed0708678d01f2
Sydney Somerfield <sydneysomerfield@gmail.com>
2017-03-17 15:09:34 -0700

    dict_board just missing calc_winner

--- tic-tac-toe/dict_board.py
+++ tic-tac-toe/dict_board.py
@@ -1,14 +1,105 @@
-# """
-# _coord_to_token = {
-#     (0, 0): 'X',
-#     'a1': 'O',
-#     '02': None,
-#     }
-# """
-#
-#
-# def main():
-#     pass
-#
-# if __name__ == '__main__':
-#     main()
+"""
+_coord_to_token = {
+    (0, 0): 'X',
+    'a1': 'O',
+    '02': None,
+    }
+"""
+
+class DictTTTBoard(object):
+    """Builds a board with dictionary coordinates"""
+
+    def __init__(self, board=None):
+        """Builds an empty board dictionary if none exists"""
+        self._coord_to_token = {}
+        if not board:
+            token = None
+            # if not -- build empty dict of coordinates:
+            for row in range(0, 3):
+                for col in range(0, 3):
+                    _coord = (row, col)
+                    self._coord_to_token.update({_coord : token})
+        else:
+            # if the board list exists
+            self._coord_to_token = board
+
+    def __repr__(self):
+        """Shows the board in a dictionary represenation
+
+        coordinate = key,  token = value
+
+        :return:
+        >>> DictTTTBoard().__repr__()
+        'DictTTTBoard({(0, 0): None, (0, 1): None, (0, 2): None, (1, 0): None, (1, 1): None, (1, 2): None, (2, 0): None, (2, 1): None, (2, 2): None})'
+        """
+        return 'DictTTTBoard({!r})'.format(
+            self._coord_to_token
+        )
+
+    def __str__(self):
+        """Pretty-print representation of the board
+
+        :return:
+        >>> str(DictTTTBoard({(0, 0): 'X', (0, 1): None, (0, 2): None, (1, 0): 'O', (1, 1): None, (1, 2): 'X', (2, 0): None, (2, 1): None, (2, 2): 'O'}))
+        '|X| | |\\r\\n|O| |X|\\r\\n| | |O|\\r\\n'
+        >>> str(DictTTTBoard())
+        '| | | |\\r\\n| | | |\\r\\n| | | |\\r\\n'
+        """
+        # creates string representation of the board for printing
+        pretty_string = '|'
+        for row, col in sorted(self._coord_to_token.keys()):
+            if col == 0 and row != 0:
+                pretty_string += '\r\n|'
+            token = self._coord_to_token.get((row, col), ' ')
+            if token is not None:
+                pretty_string += token + '|'
+            else:
+                pretty_string += ' |'
+        pretty_string += '\r\n'
+        return pretty_string
+
+    def place_token(self, x, y, token):
+        """Adds the token to the x,y coordinate in the board dictionary
+
+        :param x:
+        :param y:
+        :param token:
+        :return:
+        >>> DictTTTBoard().place_token(1, 2, 'X')
+        {(0, 0): None, (0, 1): None, (0, 2): None, (1, 0): None, (1, 1): None, (1, 2): None, (2, 0): None, (2, 1): 'X', (2, 2): None}
+        >>> DictTTTBoard({(0, 0): 'X', (0, 1): None, (0, 2): None, (1, 0): 'O', (1, 1): None, (1, 2): 'X', (2, 0): None, (2, 1): None, (2, 2): 'O'}).place_token(2, 1, 'O')
+        {(0, 0): 'X', (0, 1): None, (0, 2): None, (1, 0): 'O', (1, 1): None, (1, 2): 'O', (2, 0): None, (2, 1): None, (2, 2): 'O'}
+        """
+        board_col = x
+        board_row = y
+        # adds token to x,y coordinate key in the dictionary
+        self._coord_to_token[(board_row, board_col)] = token
+        return self._coord_to_token
+
+    def calc_winner(self):
+        """Evaluates the board to return the winner string 'X' or 'O'
+
+        If no winner, returns None
+
+        :return:
+        >>> DictTTTBoard({(0, 0): 'X', (0, 1): None, (0, 2): None, (1, 0): 'O', (1, 1): 'O', (1, 2): 'O', (2, 0): None, (2, 1): 'X', (2, 2): 'X'}).calc_winner()
+        'O'
+        """
+        # horizontal win
+        row_set = []
+        for row, col in sorted(self._coord_to_token.keys()):
+            if col == 0 and row != 0:
+                if len(set(row_set)) == 1 and row_set[0] != None:
+                    return row_set[0]
+                row_set = []
+            row_set.append(self._coord_to_token.get((row, col), None))
+        # vertical win
+
+        # diagonal win
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- tic-tac-toe/list_board.py
+++ tic-tac-toe/list_board.py
@@ -1,7 +1,7 @@
-"""
+"""Creates a Tic-Tac-Toe game board in a list of rows format:
 _rows = [
-[None, None, 'x'],
-['', 'X', ''],
+['X', ' ', 'O'],
+['O', 'X', ' '],
 ['  ', '  ', 'O'],
 ]
 """
@@ -27,7 +27,7 @@ class ListTTTBoard(object):
 
 
     def __repr__(self):
-        """
+        """Shows the tic-tac-toe board in list of lists representation
 
         :return:
         >>> ListTTTBoard().__repr__()
@@ -48,6 +48,7 @@ class ListTTTBoard(object):
         >>> str(ListTTTBoard())
         '| | | |\\r\\n| | | |\\r\\n| | | |\\r\\n'
         """
+        # creates string representation of the board for printing
         pretty_string = ''
         for row in self._rows:
             pretty_string += '|'
@@ -60,7 +61,7 @@ class ListTTTBoard(object):
 
 
     def place_token(self, x, y, token):
-        """Place an X/O token on the board
+        """Places an X/O token on the board by x, y coordinates
 
         :param x:
         :param y:
@@ -79,30 +80,45 @@ class ListTTTBoard(object):
 
 
     def calc_winner(self):
+        """Evaluates the board to return the winner string 'X' or 'O'
+
+        If no winner, returns None
+
+        :return:
+         >>> ListTTTBoard([['O', 'O', 'O'], ['O', 'X', ' '], ['X', ' ', 'X']]).calc_winner()
+         'O'
+         >>> ListTTTBoard([['X', ' ', ' '], ['X', 'O', ' '], ['X', 'O', 'O']]).calc_winner()
+         'X'
+         >>> ListTTTBoard([['A', ' ', 'O'], ['X', 'A', ' '], ['O', 'X', 'A']]).calc_winner()
+         'A'
+         >>> ListTTTBoard([['O', 'O', 'B'], [' ', 'B', 'X'], ['B', ' ', 'X']]).calc_winner()
+         'B'
+         >>> ListTTTBoard([['O', ' ', ' '], [' ', 'X', ' '], ['O', ' ', 'X']]).calc_winner()
+
+        """
         # horizontal win
         for row in self._rows:
             if len(set(row)) == 1:
-                return row
+                return row[0]
         # vertical win
-        col_to_rows = zip(*self._row)
+        col_to_rows = zip(*self._rows)
         for col in col_to_rows:
             if len(set(col)) == 1:
-                return col
-
+                return col[0]
         # diagonal win
-        # _row = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'], ['i', 'j', 'k', 'l'], ['m', 'n', 'o', 'p']]
-        col_index = len(self._row[0])
+        col_index = len(self._rows[0])
         back_diag = []
         for index in range(col_index):
-            back_diag.append(self._row[index][index])
+            back_diag.append(self._rows[index][index])
         front_diag = []
         for i in range(col_index - 1, -1, -1):
-            front_diag += self._row[index - i][i]
+            front_diag += self._rows[index - i][i]
         if len(set(back_diag)) == 1:
-            return back_diag
+            return back_diag[0]
         if len(set(front_diag)) == 1:
-            return front_diag
-        ### DECIDE ON RETURN VALUE AND CREATE A DOCTEST
+            return front_diag[0]
+        return None
+
 
 
 
