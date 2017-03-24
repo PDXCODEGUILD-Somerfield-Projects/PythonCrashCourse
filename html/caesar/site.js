0dae94e3134ec0faeecc71ba8ad11934d0e0f123
Sydney Somerfield <sydneysomerfield@gmail.com>
2017-03-24 09:39:49 -0700

    Advanced partially finished

--- html/caesar/site.js
+++ html/caesar/site.js
@@ -8,6 +8,23 @@ var letterToNumber = {
   'Y': 24, 'Z': 25, ' ': null
 };
 
+var NumberToLetter = {
+  0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F',
+  6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
+  12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R',
+  18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X',
+  24: 'Y', 25: 'Z', ' ': null
+};
+
+var letterToFrequency = {
+  'A': 73, 'B': 9, 'C': 30, 'D': 44, 'E': 130, 'F': 28,
+  'G': 16, 'H': 35, 'I': 74, 'J': 2, 'K': 3, 'L': 35,
+  'M': 25, 'N': 78, 'O': 74, 'P': 27, 'Q': 3, 'R': 77,
+  'S': 63, 'T': 93, 'U': 27, 'V': 13, 'W': 16, 'X': 5,
+  'Y': 19, 'Z': 1
+};
+
+
 
 /* This function 'encrypts' a string
 into Caesar cipher. It takes in plainStr,
@@ -87,4 +104,51 @@ function caesarDecrypt(encStr, key) {
     decryptedStr += decryptedLetter;
   }
   return decryptedStr;
+}
+
+function DecryptSansKey(encStr) {
+  var strletterToFrequency = {
+    'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0,
+    'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0,
+    'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0,
+    'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0,
+    'Y': 0, 'Z': 0
+  };
+  var encStrUpper = encStr.toUpperCase();
+  var strSpaces = 0
+  for (var i = 0; i < encStrUpper.length; i += 1) {
+    var letter = encStrUpper.charAt(i);
+    if (!(letter === ' ')) {
+      strletterToFrequency[letter] = strletterToFrequency[letter] + 1;
+    } else {
+      strSpaces += 1;
+    }
+  }
+  var bestShift = [];
+  for (var lf = 0; lf < encStrUpper.length; lf += 1) {
+    var strLetter = encStrUpper[lf];
+    var ltrLength = encStrUpper.length - strSpaces;
+    var leastChi = 0;
+    var chiShift = null;
+    var observed = strletterToFrequency[strLetter];
+    console.log('outer loop at state: ' + lf + ':' + strLetter + '\n');
+    if (!(strLetter === ' ')) {
+      for (var s = 0; s < 26; s += 1) {
+        var shiftLetter = NumberToLetter[s];
+        console.log('inner loop at state ' + shiftLetter + '\n');
+        var expected = letterToFrequency[shiftLetter] / 1000 * ltrLength;
+        var residual = observed - expected;
+        var squaredResidual = Math.pow(residual, 2);
+        var component = squaredResidual / expected;
+        if (s === 0) {
+          leastChi = component;
+          chiShift = s - letterToNumber[strLetter];
+        } else if (component < leastChi) {
+          leastChi = component;
+          chiShift = s - letterToNumber[strLetter];
+        }
+      }
+      bestShift.push(chiShift);
+    }
+  }
 }
